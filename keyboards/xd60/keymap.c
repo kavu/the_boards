3145a5f58b083c7b4e35be09dcbd2e6bdfea6af1
Max Riveiro <kavu13@gmail.com>
2018-04-07 15:35:39 +0300

    Fix XD60 build

--- keyboards/xd60/keymap.c
+++ keyboards/xd60/keymap.c
@@ -27,7 +27,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // 0: Base Layer
-  [_BASE] = KEYMAP(
+  [_BASE] = LAYOUT_ALL(
     KC_ESC,    KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSLS, KC_GRV,  \
     KC_TAB,    KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_BSPC, \
     KC_LSFT,   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX,            KC_ENT,  \
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,   KC_LALT, KC_LGUI,                             KC_SPC,                             KC_RGUI, KC_RALT, KC_LEFT,   KC_DOWN, KC_RIGHT),
 
   // 1: Function Layer
-  [_FUNC] = KEYMAP(
+  [_FUNC] = LAYOUT_ALL(
     _______,   KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,    KC_F14, \
     KC_CAPS,   KC_MPRV, KC_UP,   KC_MNXT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_DEL, \
     KC_LSFT,   KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_ENT, \
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   _______, _______,                             KC_MPLY,                            _______, _______, KC_HOME, KC_PGDOWN, KC_END),
 
   // 1: Plover GeminiPR Stenography
-  [_STEN] = KEYMAP(
+  [_STEN] = LAYOUT_ALL(
     TG(_STEN), STN_N1,  STN_N2,  STN_N3,   STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC,  XXXXXXX,  XXXXXXX, \
     STN_FN,    STN_S1,  STN_TL,  STN_PL,   STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,  XXXXXXX,           XXXXXXX, \
     XXXXXXX,   STN_S2,  STN_KL,  STN_WL,   STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,  XXXXXXX,           XXXXXXX, \
--- keyboards/xd75/config.h
+++ keyboards/xd75/config.h
@@ -27,15 +27,15 @@
 #define MIDI_ENABLE_STRICT 1
 
 /* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
+  - MIDI notes can be sent when in Music mode is on
 */
 #define MIDI_BASIC
 
 /* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
+  - MIDI notes can be added to the keymap
+  - Octave shift and transpose
+  - Virtual sustain, portamento, and modulation wheel
+  - etc.
 */
 #define MIDI_ADVANCED
 
